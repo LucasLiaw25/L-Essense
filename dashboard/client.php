7bce76ee1631fcf38c2cff11845c70da6bdd15dd
LORENZO COSTA DA SILVA <lorenzo.silva@cs.up.edu.br>
2026-04-23 10:45:18 -0300

    ERRO CORRIGIDO NO CAMINHO DA PAGINA ClientClass.php

--- dashboard/client.php
+++ dashboard/client.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-require_once __DIR__ . '../auth/ClientClass.php';
+require_once __DIR__ . '/../auth/ClientClass.php';
 
 function updateClient(Client $clientRequest, int $id) {
     $found = false;
